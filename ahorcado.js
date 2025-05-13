fdc0fab7b666c46494827b19f7709b7fda116f57
Manuel Martinez <121129488+itsmanurm@users.noreply.github.com>
2025-05-12 21:33:30 -0300

    Test No. 2: adivinar una letra correcta devuelve true

--- ahorcado.js
+++ ahorcado.js
@@ -2,9 +2,11 @@ class Ahorcado {
   constructor(palabra) {
     this.palabra = palabra;
   }
-  
-}
 
+  adivinar(letra) {
+    return this.palabra.includes(letra);
+  }
+}
 
 
 module.exports = Ahorcado;
--- ahorcado.test.js
+++ ahorcado.test.js
@@ -1,6 +1,19 @@
 const Ahorcado = require('./ahorcado');
 
+// Test No. 1: RED: No existe aún el constructor 
 test('debería crear el juego con la palabra secreta', () => {
   const juego = new Ahorcado('agil');
   expect(juego.palabra).toBe('agil');
 });
+//GREEN: crear una instancia de la clase Ahorcado 
+
+// Test No. 2: RED: No existe aún el método adivinar
+test('adivinar una letra correcta devuelve true', () => {
+  const juego = new Ahorcado('agil');
+  const resultado = juego.adivinar('a');
+  expect(resultado).toBe(true);
+});
+//GREEN: crear el método adivinar en la clase Ahorcado
+
+
+
