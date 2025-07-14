9b7a0e16d9c3faf3d2f00858e8bb66f14efaa3b8
itsmanurm <mrm291201@gmail.com>
2025-07-14 11:17:35 -0300

    Configuracion de Cypress con error en una dependencia

---
+++ ui/cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'cypress';
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+
+export default defineConfig({
+  e2e: {
+    specPattern: "**/*.feature",
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on('file:preprocessor', createBundler()); // ✅ usa bundler correcto
+      return config;
+    }
+  }
+});
